a406e4f64580447b2404eb1904765eab7113deb8
Amadri100 <andresmadrigalcampos@gmail.com>
2025-02-03 12:04:56 -0600

    Lottery proyect using classes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Welcome to this repository
 ## This repository host experiments👾
 Contains projects that are not long enough for their own repository or just experiments. 
+### Current proyects
+- An 'iq' test
+- Lottery game
+- A simple alarm
---
+++ loterry/Loterry.py
@@ -0,0 +1,36 @@
+import random
+
+class tickets:
+    #self.num = is the amount of tickets
+    def __init__(self, num):
+        self.num = num
+    def get_ticket(self):
+        my_num = random.randint(1, self.num)
+        print(f"Your ticket number is {my_num}.")
+        return my_num
+    def winner(self):
+        win_num = random.randint(1, self.num)
+        print(f"Winner number is {win_num}.")
+        return win_num
+def plai():      
+    while True:
+        maxNum = input("Select the amount of lottery numbers: ")
+        try:
+            tick = tickets(int(maxNum))
+            break
+        except ValueError:
+            print("That is not a number")
+    f = tick.get_ticket()
+    h = tick.winner()
+
+    if f == h:
+        print("Won")
+plai()
+while True:
+    print("Want to play again(y or n)")
+    ns = input(': ')
+    if ns.lower() == 'y':
+        plai()
+    else:
+        break
+
